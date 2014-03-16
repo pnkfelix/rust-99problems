e25fd6d51367bdc720748d57fb299fbaa7b21e5b
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-03-16 12:53:13 +0100

    checkpoint

--- arithmetic.rs
+++ arithmetic.rs
@@ -1,3 +1,5 @@
+use std::num::{One, Zero};
+use std_num = std::num;
 use num;
 
 /// P31 (**) Determine whether a given integer number is prime.
@@ -10,7 +12,6 @@ fn is_prime<I:num::Integer+Clone>(n:I) -> bool {
     // clearly correct, and plus, it "handles" bignums (although any
     // actual prime bignum would take too long to validate under this
     // approach).
-    use std::num::One;
     let one : I = One::one();
     let two = one + one;
     let mut i = two.clone();
@@ -28,7 +29,6 @@ fn is_prime<I:num::Integer+Clone>(n:I) -> bool {
 fn test_is_prime() {
     use num::bigint::BigUint;
     use std::from_str::FromStr;
-    use std::num::One;
     assert_eq!(true,  is_prime(7));
     assert_eq!(false, is_prime(10));
     assert_eq!(true,  is_prime(23));
@@ -41,34 +41,114 @@ fn test_is_prime() {
     // assert_eq!(true, is_prime(bigprime));
 }
 
-// P32 (**) Determine the greatest common divisor of two positive integer numbers.
-//     Use Euclid's algorithm.
-//     Example:
-//     * (gcd 36 63)
-//     9
-// 
-// P33 (*) Determine whether two positive integer numbers are coprime.
-//     Two numbers are coprime if their greatest common divisor equals 1.
-//     Example:
-//     * (coprime 35 64)
-//     T
-// 
-// P34 (**) Calculate Euler's totient function phi(m).
-//     Euler's so-called totient function phi(m) is defined as the number of positive integers r (1 <= r < m) that are coprime to m.
-// 
-//     Example: m = 10: r = 1,3,7,9; thus phi(m) = 4. Note the special case: phi(1) = 1.
-// 
-//     * (totient-phi 10)
-//     4
-// 
-//     Find out what the value of phi(m) is if m is a prime number. Euler's totient function plays an important role in one of the most widely used public key cryptography methods (RSA). In this exercise you should use the most primitive method to calculate this function (there are smarter ways that we shall discuss later).
-// 
-// P35 (**) Determine the prime factors of a given positive integer.
-//     Construct a flat list containing the prime factors in ascending order.
-//     Example:
-//     * (prime-factors 315)
-//     (3 3 5 7)
-// 
+/// P32 (**) Determine the greatest common divisor of two positive integer numbers.
+///     Use Euclid's algorithm.
+///     Example:
+///     * (gcd 36 63)
+///     9
+fn gcd<I:num::Integer>(a:I, b:I) -> I {
+    fn low_high<I:num::Integer>(a:I,b:I) -> (I,I) { if a < b { (a,b) } else { (b,a) } }
+    let (mut low, high) = low_high(a,b);
+    let mut delta = high - low;
+    while low != delta {
+        let (l, h) = low_high(low, delta);
+        delta = h - l;
+        low = l;
+    }
+    return delta;
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(9, gcd(36, 63));
+}
+
+/// P33 (*) Determine whether two positive integer numbers are coprime.
+///     Two numbers are coprime if their greatest common divisor equals 1.
+///     Example:
+///     * (coprime 35 64)
+///     T
+
+fn coprime<I:num::Integer>(a:I, b:I) -> bool {
+    gcd(a,b) == One::one()
+}
+
+#[test]
+fn test_coprime() {
+    assert_eq!(true, coprime(35, 64));
+}
+
+/// P34 (**) Calculate Euler's totient function phi(m).
+///     Euler's so-called totient function phi(m) is defined as the
+///     number of positive integers r (1 <= r < m) that are coprime to
+///     m.
+/// 
+///     Example: m = 10: r = 1,3,7,9; thus phi(m) = 4. Note the special case: phi(1) = 1.
+/// 
+///     * (totient-phi 10)
+///     4
+/// 
+///     Find out what the value of phi(m) is if m is a prime
+///     number. Euler's totient function plays an important role in
+///     one of the most widely used public key cryptography methods
+///     (RSA). In this exercise you should use the most primitive
+///     method to calculate this function (there are smarter ways that
+///     we shall discuss later).
+
+fn totient_phi<I:num::Integer+Clone>(m:I) -> I {
+    let one = One::one();
+    if m == one {
+        one
+    } else {
+        let mut count : I = Zero::zero();
+        let mut r = one.clone();
+        while r < m {
+            if coprime(r.clone(), m.clone()) {
+                count = count + one;
+            }
+            r = r + one;
+        }
+        count
+    }
+}
+
+#[test]
+fn test_totient_phi() {
+    assert_eq!(4, totient_phi(10));
+}
+
+/// P35 (**) Determine the prime factors of a given positive integer.
+///     Construct a flat list containing the prime factors in ascending order.
+///     Example:
+///     * (prime-factors 315)
+///     (3 3 5 7)
+
+fn prime_factors<I:num::Integer+Clone>(mut m:I) -> ~[I] {
+    let one : I = One::one();
+    let two = one + one;
+    let mut result = ~[];
+    let mut cand = two.clone();
+    let mut m_div_2 = m / two;
+    while cand < m_div_2 {
+        let m_div_cand = m / cand;
+        if (m_div_cand * cand) == m {
+            result.push(cand.clone());
+            m = m_div_cand;
+            m_div_2 = m / two;
+        } else {
+            cand = cand + one;
+        }
+    }
+    // could not divide further; remainder m must be prime.
+    result.push(m);
+    result
+}
+
+#[test]
+fn test_prime_factors() {
+    assert_eq!(~[3, 3, 5, 7], prime_factors(315));
+}
+
 // P36 (**) Determine the prime factors of a given positive integer (2).
 //     Construct a list containing the prime factors and their multiplicity.
 //     Example:
@@ -76,13 +156,70 @@ fn test_is_prime() {
 //     ((3 2) (5 1) (7 1))
 // 
 //     Hint: The problem is similar to problem P13.
-// 
-// P37 (**) Calculate Euler's totient function phi(m) (improved).
-//     See problem P34 for the definition of Euler's totient function. If the list of the prime factors of a number m is known in the form of problem P36 then the function phi(m) can be efficiently calculated as follows: Let ((p1 m1) (p2 m2) (p3 m3) ...) be the list of prime factors (and their multiplicities) of a given number m. Then phi(m) can be calculated with the following formula:
-// 
-//     phi(m) = (p1 - 1) * p1 ** (m1 - 1) + (p2 - 1) * p2 ** (m2 - 1) + (p3 - 1) * p3 ** (m3 - 1) + ...
-// 
-//     Note that a ** b stands for the b'th power of a.
+fn prime_factors_mult<I:num::Integer+Clone>(mut m:I) -> ~[(I,uint)] {
+    // (could multiplicity actually overflow?  Sticking with uint for clarity for now.)
+    let one : I = One::one();
+    let two = one + one;
+    let mut result = ~[];
+    let mut cand = two.clone();
+    let mut m_div_2 = m / two;
+    let mut count = 0;
+    while cand < m_div_2 {
+        let m_div_cand = m / cand;
+        if (m_div_cand * cand) == m {
+            count += 1;
+            m = m_div_cand;
+            m_div_2 = m / two;
+        } else {
+            if count > 0 {
+                result.push((cand.clone(), count));
+            }
+            cand = cand + one;
+            count = 0;
+        }
+    }
+    // could not divide further; remainder m must be prime.
+    if count > 0 {
+        result.push((cand.clone(), count));
+    }
+    result.push((m, 1));
+    result
+}
+
+#[test]
+fn test_prime_factors_mult() {
+    assert_eq!(~[(3, 2), (5, 1), (7, 1)], prime_factors_mult(315));
+}
+
+/// P37 (**) Calculate Euler's totient function phi(m) (improved).
+///     See problem P34 for the definition of Euler's totient
+///     function. If the list of the prime factors of a number m is
+///     known in the form of problem P36 then the function phi(m) can
+///     be efficiently calculated as follows: Let ((p1 m1) (p2 m2) (p3
+///     m3) ...) be the list of prime factors (and their
+///     multiplicities) of a given number m. Then phi(m) can be
+///     calculated with the following formula:
+///
+///     phi(m) = (p1 - 1) * p1 ** (m1 - 1) + (p2 - 1) * p2 ** (m2 - 1) +
+///              (p3 - 1) * p3 ** (m3 - 1) + ...
+/// 
+///     Note that a ** b stands for the b'th power of a.
+fn totient_phi_improved<I:num::Integer+Clone>(m:I) -> I {
+    let one : I = One::one();
+    let zero : I = Zero::zero();
+    let f = prime_factors_mult(m);
+    // 10 ==> (2 1) (5 1) ==> (2-1) * (2 ** 0) + (5 - 1)*(5 ** 0) 
+    //                     == 1 + 4 == 5 which does not match phi(10) given above...
+    f.move_iter().fold(zero, |b, (factor, count)| {
+        b + (factor - one) * std_num::pow(factor, count - 1)
+    })
+}
+
+#[test]
+fn test_totient_phi_improved() {
+    assert_eq!(4, totient_phi_improved(10));
+}
+
 // 
 // P38 (*) Compare the two methods of calculating Euler's totient function.
 //     Use the solutions of problems P34 and P37 to compare the algorithms. Take the number of logical inferences as a measure for efficiency. Try to calculate phi(10090) as an example.
